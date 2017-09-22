c7c12062cafa16393e6da1f2661eafd03f555d57
Martin Buchwald <martin.buchwald@mulesoft.com>
2017-09-22 16:02:18 -0300

    MULE-12799: Move ArtifactDeclaration to a new API

---
+++ src/main/java/org/mule/runtime/app/declaration/api/ConnectionElementDeclaration.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.runtime.app.declaration.api;
+
+/**
+ * A programmatic descriptor of a {@link org.mule.runtime.api.meta.model.connection.ConnectionProviderModel} configuration.
+ *
+ * @since 1.0
+ */
+public final class ConnectionElementDeclaration extends ParameterizedElementDeclaration {
+
+  public ConnectionElementDeclaration() {}
+
+  public ConnectionElementDeclaration(String extension, String name) {
+    setDeclaringExtension(extension);
+    setName(name);
+  }
+}
