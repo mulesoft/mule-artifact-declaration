011e369565004998206155f25952b891ec01b59e
Pablo Kraan <pablokraan@users.noreply.github.com>
2018-01-31 16:26:36 -0300

    MULE-14376: Annotated every element exposed on the API that have usag… (#17) (#18)

--- src/main/java/org/mule/runtime/app/declaration/api/ParameterValue.java
+++ src/main/java/org/mule/runtime/app/declaration/api/ParameterValue.java
@@ -6,6 +6,7 @@
  */
 package org.mule.runtime.app.declaration.api;
 
+import org.mule.api.annotation.NoImplement;
 import org.mule.runtime.app.declaration.api.fluent.ParameterObjectValue;
 
 /**
@@ -13,6 +14,7 @@ import org.mule.runtime.app.declaration.api.fluent.ParameterObjectValue;
  *
  * @since 1.0
  */
+@NoImplement
 public interface ParameterValue {
 
   /**
