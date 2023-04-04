f3b1af535661cc86462cb0fa7fcba6220a5f8bd4
Rodrigo Merino <elrodro83@users.noreply.github.com>
2023-04-04 18:11:40 -0300

    W-12724662: Generate a module-info.java for mule-artifact-declaration module (#96)

---
+++ mule-artifact-declaration-persistence/src/main/java/module-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+/**
+ * Mule Artifact Declaration persistence layer.
+ * 
+ * @moduleGraph
+ * @since 1.6
+ */
+module org.mule.runtime.artifact.declaration.persistence {
+
+  requires org.mule.runtime.artifact.declaration;
+
+  requires com.google.gson;
+
+  exports org.mule.runtime.app.declaration.serialization.api;
+
+}
---
+++ mule-artifact-declaration/src/main/java/module-info.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+/**
+ * Mule Artifact Declaration.
+ * 
+ * @deprecated use {@code org.mule.runtime.artifact.ast} instead.
+ * 
+ * @moduleGraph
+ * @since 1.6
+ */
+module org.mule.runtime.artifact.declaration {
+  
+  requires org.mule.runtime.api.annotations;
+  requires org.mule.runtime.metadata.model.api;
+  
+  requires org.apache.commons.lang3;
+  
+  exports org.mule.runtime.app.declaration.api;
+  exports org.mule.runtime.app.declaration.api.fluent;
+  exports org.mule.runtime.app.declaration.api.component.location;
+
+  // Allow introspection for serialization/deserialization by Gson
+  opens org.mule.runtime.app.declaration.api to
+      com.google.gson;
+  opens org.mule.runtime.app.declaration.api.fluent to
+      com.google.gson;
+
+}
